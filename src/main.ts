0381c3b44179d1f20a9fcbfb8cbebfe508b434e8
Tom Chauvel <tom.chauvel@hotmail.com>
2022-07-25 00:18:08 +0200

    problème safari firefox
    ajout d'une width et d'une height dynamique avec js

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,14 @@ app.config.globalProperties.globalPath = ref([]);
 
 app.use(router);
 app.mount('#app');
+
+const appResize = () => {
+  const vh = window.innerHeight;
+  const vw = window.innerWidth;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty('--vw', `${vw}px`);
+}
+
+window.addEventListener('resize', appResize)
+
+appResize()
