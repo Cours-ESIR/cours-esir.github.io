6611aee32f828ad48569e052ec07daad113f29a6
Tom Chauvel <tom.chauvel@hotmail.com>
2022-07-25 14:29:42 +0200

    add routeur

--- assets/AboutView.7acb05bd.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as r,e as c}from"./index.ae5763a4.js";const t={};function a(o,n){return r(),c("div")}var s=e(t,[["render",a]]);export{s as default};
--- assets/EditView.332bee71.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as r,e as c}from"./index.ae5763a4.js";const t={};function a(n,o){return r(),c("div")}var s=e(t,[["render",a]]);export{s as default};
