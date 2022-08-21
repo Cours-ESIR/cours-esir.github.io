d7b10500de0433b075752bf4b70262c4210f7c2f
AquaBx <tom.chauvel@hotmail.com>
2022-08-21 20:58:03 +0200

    planning en composant

---
+++ src/components/Main/Planning_Component.vue
@@ -0,0 +1,117 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+	planning: [];
+}>();
+
+
+</script>
+
+
+<template>
+
+        <div-planning>
+            <rowheader>
+                <column>Lundi</column>
+                <column>Mardi</column>
+                <column>Mercredi</column>
+                <column>Jeudi</column>
+                <column>Vendredi</column>
+                <column>Samedi</column>
+                <column>Dimanche</column>
+            </rowheader>
+            <rowbody>
+
+                
+                <column class="events-list" v-for="day of planning">
+                    <div v-for="event of day" :style="{top:'calc( 50px *'+event.start+')',height: 'calc(50px *'+ event.duration+')'}"></div>
+                    
+                </column>
+            </rowbody>
+            <row-legend>
+                <div class="lines" v-for="y in 24" :style="{top:'calc( 50px *'+y+')'}">
+                    <span class="text">{{y%24}}h00</span>
+                    <span class="line"></span>
+                </div>
+            </row-legend>
+        </div-planning>
+
+
+
+</template>
+
+
+
+<style scoped>
+    div-planning{
+        width:100%;
+        display: grid;
+        position:relative;
+    }
+
+    div-planning > * {
+        display: block;
+        width:100%;
+        height: auto;
+        display: grid;
+        grid-auto-flow: column;
+        grid-template-columns: repeat(7, 1fr);
+    }
+
+    div-planning > rowheader{
+        position: sticky;
+        padding-top:40px;
+        top:0;
+        border-bottom:1px solid var(--text);
+        width: 100%;
+        background-color: var(--background);
+        z-index: 5;
+        text-align: center;
+    }
+
+    div-planning > row-legend{
+        position: absolute;
+        height: calc( 50px * 24 );
+        top:66px;
+        z-index: -1;
+    }
+
+    div-planning > row-legend > .lines{
+        position: absolute;
+        right:0;
+        width: 100%;
+    }
+    
+    div-planning > row-legend > .lines > .text{
+        transform: translate(-150%,-50%);
+        position: absolute;
+    }
+    div-planning > row-legend > .lines > .line{
+        border-top: 1px solid var(--text);
+        top:0;
+        display: block;
+        width:100%;
+    }
+
+    column {
+        width: 100%;
+    }
+
+    column > * {
+        background-color: var(--blue);
+    }
+
+    .events-list {
+        position: relative;
+        height: calc( 50px * 24 );
+    }
+    .events-list > * {
+        --decalage : 0.1em;
+        border-radius: 0.6em;
+        margin: 0 var(--decalage);
+        width: calc(100% - var(--decalage)*2);
+        position: absolute;
+        left:0;
+        background-color: var(--blue);
+    }
+</style>
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+    import Planning_Component from "../components/Main/Planning_Component.vue";
 
     let planning = [
         [{"start":15.5,"duration":0.75}],
@@ -6,76 +7,14 @@
         [{"start":15.5,"duration":0.75}],
         [{"start":15.5,"duration":0.75}],
         [{"start":15.5,"duration":0.75}],
-        [{"start":15.5,"duration":0.75}],
-        [{"start":15.5,"duration":0.75}]
+        [{"start":0,"duration":1.75}],
+        [{"start":23,"duration":1}]
     ]
 
 </script>
 
 <template>
     <router style="display:block;">
-        <div-planning>
-            <rowheader>
-                <column>Lundi</column>
-                <column>Mardi</column>
-                <column>Mercredi</column>
-                <column>Jeudi</column>
-                <column>Vendredi</column>
-                <column>Samedi</column>
-                <column>Dimanche</column>
-            </rowheader>
-            <rowbody>    
-                <column class="events-list" v-for="day of planning">
-                    <div v-for="event of day" :style="{top:'calc(200vh / 23 *'+event.start+')',height: 'calc(200vh / 23 *'+ event.duration+')'}"></div>
-                </column>
-            </rowbody>
-        </div-planning>
+        <Planning_Component :planning="planning"></Planning_Component>
     </router>
 </template>
-
-<style scoped>
-    div-planning{
-        width:100%;
-        display: grid;
-    }
-
-    div-planning > * {
-        display: block;
-        width:100%;
-        height: auto;
-        display: grid;
-        grid-auto-flow: column;
-        grid-template-columns: repeat(7, 1fr);
-    }
-
-    div-planning > rowheader{
-        position: sticky;
-        padding-top:40px;
-        top:0;
-        border-bottom:1px solid var(--text);
-        width: 100%;
-        background-color: var(--background);
-        z-index: 5;
-        text-align: center;
-    }
-
-    column:not(column:nth-child(2n-1)){
-        border-left:1px solid var(--text);
-        border-right:1px solid var(--text);
-    }
-
-    column {
-        width: 100%;
-    }
-
-    .events-list {
-        position: relative;
-        height: calc(200vh * 24/23);
-    }
-    .events-list > * {
-        width: 100%;
-        position: absolute;
-        left:0;
-        background-color: blue;
-    }
-</style>
