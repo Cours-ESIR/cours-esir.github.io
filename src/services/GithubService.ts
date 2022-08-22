d993f233cf2fc12a25405c85e0f0e95ceda7d62a
AquaBx <tom.chauvel@hotmail.com>
2022-08-22 16:12:06 +0200

    lien manquant

--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -1,10 +1,14 @@
 import type { RepositoryTree } from '@/types/Github';
 
 export default class GithubService {
-	public static readonly GITHUB_API_URL = 'https://api.github.com/repos/Cours-ESIR/Cours/contents';
-	public static readonly GITHUB_CONTRIB_URL = 'https://api.github.com/repos/Cours-ESIR/Cours/contributors';
-	public static readonly GITHUB_TREE_URL = '/tree.json';
-	public static readonly GITHUB_DATA_URL = 'https://raw.githubusercontent.com/Cours-ESIR/Cours/main/';
+	public static readonly GITHUB_API_URL =
+		'https://api.github.com/repos/Cours-ESIR/Cours/contents';
+	public static readonly GITHUB_CONTRIB_URL =
+		'https://api.github.com/repos/Cours-ESIR/Cours/contributors';
+	public static readonly GITHUB_TREE_URL =
+		'https://api.github.com/repos/Cours-ESIR/Cours/git/trees/main?recursive=1';
+	public static readonly GITHUB_DATA_URL =
+		'https://raw.githubusercontent.com/Cours-ESIR/Cours/main/';
 
 	public static async fetchTree(): Promise<RepositoryTree> {
 		let data = await fetch(this.GITHUB_TREE_URL);
